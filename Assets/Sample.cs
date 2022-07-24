19738a39dd0f2314e8a94686fc783cc19ccc4eb0
山内 悠佑 <yuusuke/yamauchi@i-standard.jp>
2022-07-25 08:35:07 +0900

    Sample.csにコメントを入れました。

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -13,7 +13,18 @@ public class Sample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //(x,y,z)//
+        /*
+         * GameObjectの位置・回転・拡縮はtransformを操作します。
+         * 位置情報はtransformのpositionで、positionはVector3型になっています。
+         * 中の要素は(x,y,z)でそれぞれの要素ごとに操作することも可能です。
+         * その場合は以下の手順をふみます。
+         * 
+         * Vector3 pos = transform.position
+         * pos.z += 2.0f;
+         * transform.position = pos;
+         * 
+         * 現在のままでは3D空間の前方にしか移動することは出来ません。
+        */
         transform.position += Vector3.forward * 2.0f;
     }
 }
