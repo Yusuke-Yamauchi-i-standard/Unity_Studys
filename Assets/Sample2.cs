dadc5da6b003e5397a46d394e834a25cb398b3ac
山内 悠佑 <yuusuke/yamauchi@i-standard.jp>
2022-07-25 08:42:14 +0900

    Sample2.csにコメントを追加しました。

--- Assets/Sample2.cs
+++ Assets/Sample2.cs
@@ -13,6 +13,22 @@ public class Sample2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+         *
+         * GameObjectを回転させるにはrotationを利用します。
+         * rotationはQuaternion型です。
+         * 
+         * Quaternionは3D空間での計算によく利用されます。
+         * 一般的なx,y,zの軸で回転させることをEuler(オイラー)角と言います。
+         * Quaternionはかなり特殊で、x,y,zの値をそのまま入れて利用すると失敗します。
+         * そこでUnityではQuaternion.Eulerを利用します。
+         * このメソッドはEuler角を利用してQuarternionを作成してくれる機能になります。
+         * 
+         * Quaternionのもう一つの特徴として、回転情報を合わせるには掛け算しか利用できない点です。
+         * 誤って足し算は利用しない様にしましょう。
+         * 
+        */
+
         transform.rotation *= Quaternion.Euler(0.5f, 0.0f, 0.0f);
         //transform.rotation = transform.rotation * Quaternion.Euler(0.5f, 0.0f, 0.0f);
     }
